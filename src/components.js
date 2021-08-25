740a5f0a3b58174f47eed153501ccce70374f5dc
chenkai0520 <k_0520@qq.com>
2021-08-25 16:18:30 +0800

    perf: update dependencies

--- src/components.js
+++ src/components.js
@@ -1,17 +1,14 @@
 /* Do not modify the automatically generated code */
 import Button from '@/components/button';
 import Label from '@/components/label';
-import Select from '@/components/select';
 
 const components = [
   Button,
   Label,
-  Select,
 ];
 
 export {
   components,
   Button,
   Label,
-  Select,
 };
