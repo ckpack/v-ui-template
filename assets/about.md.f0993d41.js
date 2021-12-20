375bb139205e3e8437f3e3bfab4ac9b9b6a49a7a
ckvv <ckvv@users.noreply.github.com>
2021-12-20 10:18:19 +0000

    deploy: 520e05bb89b98357225117151f954ccb327b7065

---
+++ assets/about.md.f0993d41.js
@@ -0,0 +1,1 @@
+import{_ as t,c as e,o as a,a as o}from"./app.feb75059.js";const m='{"title":"","description":"","frontmatter":{},"relativePath":"about.md","lastUpdated":1639995432605}',s={},r=o("p",null,"\u5173\u4E8E v-ui-template",-1),c=[r];function n(p,_,d,i,l,u){return a(),e("div",null,c)}var h=t(s,[["render",n]]);export{m as __pageData,h as default};
--- assets/en_about.md.6428e3a1.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,c as e,o as a,a as o}from"./app.eca0e492.js";const m='{"title":"","description":"","frontmatter":{},"relativePath":"en/about.md","lastUpdated":1639994677519}',s={},c=o("p",null,"About v-ui-template",-1),n=[c];function r(p,_,d,i,l,u){return a(),e("div",null,n)}var h=t(s,[["render",r]]);export{m as __pageData,h as default};
