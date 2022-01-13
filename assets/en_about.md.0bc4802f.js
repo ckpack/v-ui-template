54898bf5ca49f8482289196ba9afc13afc87c1e7
ckvv <ckvv@users.noreply.github.com>
2022-01-13 11:33:49 +0000

    deploy: b55c08131a482ffc9b7933fd3c8f347d970a4b18

---
+++ assets/en_about.md.0bc4802f.js
@@ -0,0 +1,1 @@
+import{_ as t,c as e,o as a,a as o}from"./app.b48120a1.js";const m='{"title":"","description":"","frontmatter":{},"relativePath":"en/about.md","lastUpdated":1642073602466}',s={},n=o("p",null,"About v-ui-template",-1),r=[n];function c(p,_,d,i,l,u){return a(),e("div",null,r)}var b=t(s,[["render",c]]);export{m as __pageData,b as default};
