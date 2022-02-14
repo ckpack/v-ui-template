f250e86591df5ee3338fdbd68968dfedb7aa70ee
chenkai0520 <k_0520@qq.com>
2022-02-14 16:04:01 +0800

    fix: external error

--- scripts/vite.es.js
+++ scripts/vite.es.js
@@ -3,7 +3,7 @@ import { build } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 const { dependencies } = JSON.parse(fs.readFileSync('./package.json'));
-const external = ['vue', Object.keys(dependencies || [])];
+const external = ['vue', ...Object.keys(dependencies || [])];
 const outDir = 'es';
 const preserveModulesRoot = 'src';
 
