6b162820a4ec28506fa7ae95d3ae630de45d6895
ckvv <ckvv@users.noreply.github.com>
2022-04-11 08:07:27 +0000

    deploy: 8e80e065299b8f0b3208a0006569c13f0923fa0b

---
+++ assets/en_about.md.fb131c61.lean.js
@@ -0,0 +1,1 @@
+import{_ as t,y as e,x as a,D as o}from"./plugin-vue_export-helper.54ce3857.js";const m='{"title":"","description":"","frontmatter":{},"relativePath":"en/about.md","lastUpdated":1649664414221}',s={},n=o("p",null,"About v-ui-template",-1),r=[n];function c(_,p,d,i,l,u){return a(),e("div",null,r)}var h=t(s,[["render",c]]);export{m as __pageData,h as default};
